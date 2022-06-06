82ca9888f328ae57082f1c65ba866af06c9a7bc9
HuntClauss <hunt.clauss@protonmail.com>
2022-06-06 09:09:23 +0200

    config

---
+++ engine/Config.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace engine;
+
+final class Config {
+	const DB_HOSTNAME = 'localhost';
+	const DB_USERNAME = "clauss";
+	const DB_PASSWORD = "zaq1@WSX";
+	const DB_NAME = "szyper";
+}
+
+
